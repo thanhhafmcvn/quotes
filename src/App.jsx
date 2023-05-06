bdf37e7ba12f50c3d18e1db15b5b6e11568417ec
mlucastero <thanhhafmcvn@outlook.com.vn>
2023-05-06 09:07:09 +0700

    added author-searching feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React, {useState, useEffect} from 'react'
 import "./App.css";
 import Quote from './components/Quote'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
+
 
 function App() {
 
-  return <div className="App">
+  return (
+    <div className="w-screen h-screen bg-black m-0 p-0">
     <Quote />
-  </div>;
+  </div>
+  )
 }
 
 export default App;
--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,32 +1,39 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Quote = () => {
+  const [quoteData, setQuoteData] = useState([]);
+  const [authorShown, setAuthorShown] = useState(false);
+  const [authorName, setAuthorName] = useState("");
   const API_URL = "https://api.quotable.io/quotes/random";
-  const [quoteData, setQuoteData] = useState([])
-  const [showAuthor, setShowAuthor] = useState(false);
+  const SEARCH_URL = `https://google.com/search?q=${authorName}`;
   setTimeout(() => {
-    setShowAuthor(true)
-  },3000)
+    setAuthorShown(true);
+  }, 3000);
   const fetchQuote = async () => {
     const response = await axios.get(API_URL);
     const data = response.data;
     setQuoteData(data);
-  }
-  useEffect(
-    () => { fetchQuote() }, []
-  )
+    setAuthorName(data[0]?.author)
+  };
+  useEffect(() => {
+    fetchQuote();
+  }, []);
 
   return (
-    <div className="w-screen h-screen bg-black flex flex-1 justify-center items-center flex-col p-[200px]">
+    <div className="w-screen h-screen flex flex-1 justify-center items-center flex-col p-[200px]">
       <div className=" flex flex-1 justify-center items-center flex-col ">
-        <h1 className='text-white text-center font-sans font-bold text-[50px] animate-fade w-100'>{quoteData[0]?.content}</h1>
-        <div className={'text-white font-extralight text-[30px] mt-[40px]'}>
-          <h3 className={showAuthor ? 'opacity-1' : 'opacity-0'}>{quoteData[0]?.author}</h3>
+        <h1 className="text-white text-center font-sans font-bold text-[50px] animate-fade w-100">
+          {quoteData[0]?.content}
+        </h1>
+        <div className={"text-white font-extralight text-[30px] mt-[40px]"}>
+          <h3 className={authorShown ? "opacity-1 cursor-pointer" : "opacity-0"} onClick={() => window.open(SEARCH_URL, '_blank')}>
+            {quoteData[0]?.author}
+          </h3>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Quote
+export default Quote;
