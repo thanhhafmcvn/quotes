e92d909cadee38bd1e5ab53f4cda3d60101d7311
Matthew Le <matthewle@192.168.2.15>
2026-05-23 22:56:23 +0700

    Responsive design added

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -21,15 +21,20 @@ const Quote = () => {
   }, []);
 
   return (
-    <div className="w-screen h-screen flex flex-1 justify-center items-center flex-col p-[200px]">
-      <div className=" flex flex-1 justify-center items-center flex-col ">
-        <h1 className="text-white text-center font-sans font-bold text-[50px] animate-fade w-100">
+    <div className="min-h-screen w-full flex flex-col justify-center items-center px-6 sm:px-12 bg-black">
+      <div className=" w-full max-w-3xl mx-auto text-center">
+        <h1 className="text-white text-left font-sans font-black text-xl sm:text-3xl md:text-3xl lg:text-5xl animate-fade w-100" id="content">
           {quoteData[0]?.content}
         </h1>
-        <div className="text-white font-extralight text-[30px] mt-[40px]">
-          <h3 className={authorShown ? "opacity-1 cursor-pointer" : "opacity-0"} onClick={() => window.open(SEARCH_URL, '_blank')}>
+        <div className="mt-4 md:mt-6 text-base sm:text-sm md:text-lg lg:text-xl text-white tracking-widest font-light leading-normal break-words">
+          <h3 className={authorShown ? "opacity-1 cursor-pointer" : "opacity-0"} onClick={() => window.open(SEARCH_URL, '_blank')} id="author">
             {quoteData[0]?.author}
           </h3>
+        </div>
+        <div>
+            <h4 className="mt-3 md:mt-5 text-[7px] sm:text-[7px] md:text-[7px] lg:text-[10px] font-semibold text-slate-400 uppercase tracking-widest">
+            Click the name to find out.
+          </h4>
         </div>
       </div>
     </div>
